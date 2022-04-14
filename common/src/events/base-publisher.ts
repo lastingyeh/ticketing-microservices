25aa6b43ae3b05e34252309bb44865376f2d3401
lastingyeh <lastingyeh@gmail.com>
2022-04-15 06:19:40 +0800

    Updates

--- common/src/events/base-publisher.ts
+++ common/src/events/base-publisher.ts
@@ -9,7 +9,7 @@ interface Event {
 abstract class Publisher<T extends Event> {
 	abstract subject: T['subject'];
 
-	private client: Stan;
+	protected client: Stan;
 
 	constructor(client: Stan) {
 		this.client = client;
